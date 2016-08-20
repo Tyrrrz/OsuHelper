05fd7afb039f09660401160630bf978c5e7d0d43
Alexey Golub <tyrrrrrr@gmail.com>
2016-08-20 20:55:29 +0300

    Added SetupViewModel, defined Settings and bound some controls to the view model

--- OsuHelper/Settings.cs
+++ OsuHelper/Settings.cs
@@ -10,11 +10,29 @@ using NegativeLayer.Settings;
 
 namespace OsuHelper
 {
-    public class Settings : SettingsManager
+    public sealed class Settings : SettingsManager
     {
+        private string _userName;
+        private string _apiKey;
+
         public static Settings Default { get; } = new Settings();
 
-        public string UserName { get; set; }
-        public string APIKey { get; set; }
+        public string UserName
+        {
+            get { return _userName; }
+            set { Set(ref _userName, value); }
+        }
+
+        public string APIKey
+        {
+            get { return _apiKey; }
+            set { Set(ref _apiKey, value); }
+        }
+
+        public Settings()
+        {
+            // Load settings on initialization
+            TryLoad();
+        }
     }
 }
--- OsuHelper/ViewModels/SetupViewModel.cs
+++ OsuHelper/ViewModels/SetupViewModel.cs
@@ -7,10 +7,46 @@
 // ------------------------------------------------------------------ 
 
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
 
 namespace OsuHelper.ViewModels
 {
     public class SetupViewModel : ViewModelBase
     {
+        public Settings Settings => Settings.Default;
+
+        // Commands
+        public RelayCommand SaveCommand { get; }
+        public RelayCommand LoadCommand { get; }
+        public RelayCommand ResetDefaultsCommand { get; }
+
+        public SetupViewModel()
+        {
+            // Commands
+            SaveCommand = new RelayCommand(Save, () => !Settings.IsSaved);
+            LoadCommand = new RelayCommand(Load, () => !Settings.IsSaved);
+            ResetDefaultsCommand = new RelayCommand(ResetDefaults);
+        }
+
+        private void Save()
+        {
+            Settings.Save();
+
+            SaveCommand.RaiseCanExecuteChanged();
+            LoadCommand.RaiseCanExecuteChanged();
+        }
+
+        private void Load()
+        {
+            Settings.Load();
+
+            SaveCommand.RaiseCanExecuteChanged();
+            LoadCommand.RaiseCanExecuteChanged();
+        }
+
+        private void ResetDefaults()
+        {
+            Settings.Reset();
+        }
     }
 }

dc785b6fed95d99c9e7fab00045373b115a5771e
Alexey Golub <tyrrrrrr@gmail.com>
2017-04-09 20:56:21 +0300

    Parse userid from profile url

--- OsuHelper.Impl/Services/FileSettingsService.cs
+++ OsuHelper.Impl/Services/FileSettingsService.cs
@@ -1,16 +1,33 @@
 ﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using OsuHelper.Models;
+using Tyrrrz.Extensions;
 using Tyrrrz.Settings;
 
 namespace OsuHelper.Services
 {
     public class FileSettingsService : SettingsManager, ISettingsService
     {
-        public string UserId { get; set; }
+        private string _userId;
+
+        public string UserId
+        {
+            get { return _userId; }
+            set
+            {
+                string fromUrl = Regex.Match(value, @".*?.ppy.sh/\w/([\w\d]+)").Groups[1].Value;
+                _userId = fromUrl.IsNotBlank() ? fromUrl : value;
+            }
+        }
+
         public string ApiRoot { get; set; } = "https://osu.ppy.sh/api/";
+
         public string ApiKey { get; set; }
+
         public GameMode GameMode { get; set; } = GameMode.Standard;
+
         public bool ShouldDownloadWithoutVideo { get; set; }
+
         public IEnumerable<BeatmapRecommendation> LastRecommendations { get; set; }
 
         public FileSettingsService()

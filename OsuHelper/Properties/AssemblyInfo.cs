7f8e4dc5c11153109f061ac8b18c7c14f95da1f6
Alexey Golub <Tyrrrrrr@gmail.com>
2017-12-31 01:26:51 +0200

    Update version

--- OsuHelper/Properties/AssemblyInfo.cs
+++ OsuHelper/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Windows;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: AssemblyVersion("2.0")]
-[assembly: AssemblyFileVersion("2.0")]
+[assembly: AssemblyVersion("2.0.1")]
+[assembly: AssemblyFileVersion("2.0.1")]

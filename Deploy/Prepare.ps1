f678093b0843aa6bd21d3ebdc4a12e630e6206e4
Alexey Golub <Tyrrrrrr@gmail.com>
2018-03-20 20:43:45 +0200

    Rename output archive in deployment to avoid issues with invalid characters

--- Deploy/Prepare.ps1
+++ Deploy/Prepare.ps1
@@ -1,4 +1,4 @@
 New-Item "$PSScriptRoot\bin" -ItemType Directory -Force
 $files = @()
 $files += Get-ChildItem -Path "$PSScriptRoot\..\OsuHelper\bin\Release\*" -Include "*.exe", "*.dll", "*.config"
-$files | Compress-Archive -DestinationPath "$PSScriptRoot\bin\osu!helper.zip" -Force
+$files | Compress-Archive -DestinationPath "$PSScriptRoot\bin\osu.helper.zip" -Force

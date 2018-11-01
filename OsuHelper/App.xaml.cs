2a017c840c00d505403b87d8a765bb9b55ebbbb0
Oleksii Holub <Oleksii.Holub@3shape.com>
2018-11-01 14:48:48 +0200

    Show dispatcher exceptions to the user

--- OsuHelper/App.xaml.cs
+++ OsuHelper/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Threading;
 using GalaSoft.MvvmLight.Threading;
 
 namespace OsuHelper
@@ -10,14 +11,19 @@ namespace OsuHelper
             DispatcherHelper.Initialize();
         }
 
-        private void App_OnStartup(object sender, StartupEventArgs e)
+        private void App_OnStartup(object sender, StartupEventArgs args)
         {
             Container.Init();
         }
 
-        private void App_OnExit(object sender, ExitEventArgs e)
+        private void App_OnExit(object sender, ExitEventArgs args)
         {
             Container.Cleanup();
+        }
+
+        private void App_OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs args)
+        {
+            MessageBox.Show(args.Exception.ToString(), "Error occured", MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
 }

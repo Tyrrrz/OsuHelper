07af471ac58e1c19ee951a9984ed840c4c71ae9e
Alexey Golub <Tyrrrrrr@gmail.com>
2019-02-17 19:37:14 +0200

    Add license file to released artifacts

--- Deploy/Prepare.ps1
+++ Deploy/Prepare.ps1
@@ -1,4 +1,5 @@
 New-Item "$PSScriptRoot\bin" -ItemType Directory -Force
 $files = @()
+$files += Get-Item -Path "$PSScriptRoot\..\License.txt"
 $files += Get-ChildItem -Path "$PSScriptRoot\..\OsuHelper\bin\Release\*" -Include "*.exe", "*.dll", "*.config"
 $files | Compress-Archive -DestinationPath "$PSScriptRoot\bin\osu.helper.zip" -Force

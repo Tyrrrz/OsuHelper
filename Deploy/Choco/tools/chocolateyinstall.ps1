4827cbb8fda22783b29d473a24b22178206efa09
Alexey Golub <Tyrrrrrr@gmail.com>
2019-04-07 01:19:59 +0300

    Add deploy via chocolatey

---
+++ Deploy/Choco/tools/chocolateyinstall.ps1
@@ -0,0 +1,7 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageArgs = @{
+  packageName = $env:ChocolateyPackageName
+  unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  url = 'https://github.com/Tyrrrz/OsuHelper/releases/download/2.0.2/osu.helper.zip'
+}
+Install-ChocolateyZipPackage @packageArgs
--- Deploy/Prepare.ps1
+++ Deploy/Prepare.ps1
@@ -1,5 +1,16 @@
-New-Item "$PSScriptRoot\bin" -ItemType Directory -Force
+# --- PORTABLE ---
+
+# Get files
 $files = @()
 $files += Get-Item -Path "$PSScriptRoot\..\License.txt"
 $files += Get-ChildItem -Path "$PSScriptRoot\..\OsuHelper\bin\Release\*" -Include "*.exe", "*.dll", "*.config"
-$files | Compress-Archive -DestinationPath "$PSScriptRoot\bin\osu.helper.zip" -Force
+
+# Pack into archive
+New-Item "$PSScriptRoot\Portable\bin" -ItemType Directory -Force
+$files | Compress-Archive -DestinationPath "$PSScriptRoot\Portable\bin\OsuHelper.zip" -Force
+
+# --- CHOCOLATEY ---
+
+# Create package
+New-Item "$PSScriptRoot\Choco\bin\" -ItemType Directory -Force
+choco pack $PSScriptRoot\Choco\osuhelper.nuspec --out $PSScriptRoot\Choco\bin\

79af7c876db1b8f637fdec31e2f8f7e8ef655083
Oleksii Holub <Oleksii.Holub@3shape.com>
2019-04-09 18:03:30 +0300

    Optimize chocolatey package

--- Deploy/Choco/tools/chocolateyinstall.ps1
+++ Deploy/Choco/tools/chocolateyinstall.ps1
@@ -1,7 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop';
+
+# Install package
 $packageArgs = @{
-  packageName = $env:ChocolateyPackageName
-  unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-  url = 'https://github.com/Tyrrrz/OsuHelper/releases/download/2.0.2/osu.helper.zip'
+    packageName   = $env:ChocolateyPackageName
+    unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+    url           = 'https://github.com/Tyrrrz/OsuHelper/releases/download/2.0.2/osu.helper.zip'
 }
 Install-ChocolateyZipPackage @packageArgs
+
+# Mark the executable as GUI
+New-Item (Join-Path unzipLocation "osu!helper.exe.gui") -ItemType File -Force
